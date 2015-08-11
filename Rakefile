5ca01addea85f97382b2fd37ae7d0c9a3f980381
ashes999 <ashes999@yahoo.com>
2015-08-11 10:10:05 -0400

    Move files around.

--- Rakefile
+++ Rakefile
@@ -1,10 +1,8 @@
-require 'json'
-require 'fileutils'
+require './src/builder'
 
 desc "Create the website based on data and files."
 task :build do
-  games = JSON.parse(File.read('data/games.yaml'))
-  games = games['games']  
+  Builder.new.build
 end
 
 # set default task: build the website
--- src/builder.rb
+++ src/builder.rb
@@ -1,11 +1,11 @@
 require 'json'
 
-class Main
+class Builder
 
   DATABASE_FILE = 'data/games.yaml'
   TEMPLATE_DIRECTORY = 'templates'
 
-  def run
+  def build
     verify_files_exist
     generate_data
   end
@@ -14,12 +14,12 @@ class Main
 
   def verify_files_exist
     raise "#{DATABASE_FILE} not found" unless File.exist?(DATABASE_FILE)
-    raise "#{TEMPLATE_DIRECTORY} directory not found" unless Dir.exists?(TEMPLATE_DIRECTORY)
+    raise "#{TEMPLATE_DIRECTORY} directory not found" unless Dir.exist?(TEMPLATE_DIRECTORY)
   end
 
   def generate_data
     games = JSON.parse(File.read(DATABASE_FILE))
-    games = games['list']
+    games = games['games']
     games.each do |g|
       puts "#{g['name']} => #{game_name_to_token(g['name'])}, #{g['platforms']}"
     end
@@ -29,5 +29,3 @@ class Main
     return name.gsub(' ', '-').gsub('_', '-').downcase.strip.chomp
   end
 end
-
-Main.new.run
