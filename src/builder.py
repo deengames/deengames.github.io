1942dcb40e8a0fea9b9a41dcdd3aba18ee6999d4
ashes999 <ashes999@yahoo.com>
2017-07-07 18:27:06 -0400

    Build on Windows, too. Remove hardcoded platform-specific use of os.sep.
    Former-commit-id: d84118dd6d3dbe5013d78a16f802d2449a4728d9

--- src/builder.py
+++ src/builder.py
@@ -17,9 +17,9 @@ class Builder:
 
     OUTPUT_DIR = 'bin' # where to build to
     DATA_DIR = 'data' # where our data files are
-    DATABASE_FILE = "{0}/games.json".format(DATA_DIR)
-    STATIC_PAGES_DIR = "{0}/static_pages".format(DATA_DIR)
-    IMAGES_DIR = "{0}/images".format(DATA_DIR)
+    DATABASE_FILE = os.path.join(DATA_DIR, "games.json")
+    STATIC_PAGES_DIR = os.path.join(DATA_DIR, "static_pages")
+    IMAGES_DIR = os.path.join(DATA_DIR, "images")
     GAMES_DIR = "games"
     GUIDES_DIR = "guides"
 
@@ -32,11 +32,11 @@ class Builder:
     INDEX_PAGE = 'index.html' # the home index page; this is also the layout.
     NAVBAR_LINKS_PLACEHOLDER = '<!-- DG navbar links -->' # Where the navbar list of pages goes
     # Snippets where we put in real data (eg. game page URLs)
-    NAVBAR_LINK_SNIPPET = "{0}/snippets/navbar_link.html".format(TEMPLATE_DIRECTORY)
-    JUMBOTRON_SNIPPET = "{0}/snippets/jumbotron.html".format(TEMPLATE_DIRECTORY)
-    SCREENSHOTS_SNIPPET = "{0}/snippets/screenshots.html".format(TEMPLATE_DIRECTORY)
+    NAVBAR_LINK_SNIPPET = os.path.join(TEMPLATE_DIRECTORY, "snippets", "navbar_link.html")
+    JUMBOTRON_SNIPPET = os.path.join(TEMPLATE_DIRECTORY, "snippets", "jumbotron.html")
+    SCREENSHOTS_SNIPPET = os.path.join(TEMPLATE_DIRECTORY, "snippets", "screenshots.html")
     CONTENT_PLACEHOLDER = '<!-- DG content -->' # Where in the template we fill in the page content
-    GAME_PAGE_TEMPLATE = "{0}/game.html".format(TEMPLATE_DIRECTORY)
+    GAME_PAGE_TEMPLATE = os.path.join(TEMPLATE_DIRECTORY, "game.html")
 
     GOOGLE_PLAY_PATH = 'https://play.google.com/store/apps/details?id=' # URL for Google Play
 
@@ -81,14 +81,14 @@ class Builder:
         # Copy over CSS, fonts, JS, and the index page, plus site-wide images, etc.
         distutils.dir_util.copy_tree(Builder.TEMPLATE_DIRECTORY, Builder.OUTPUT_DIR)
         for exclusion in Builder.TEMPLATE_EXCLUSIONS:                
-            shutil.rmtree("{0}/{1}".format(Builder.OUTPUT_DIR, exclusion), True)
+            shutil.rmtree(os.path.join(Builder.OUTPUT_DIR, exclusion), True)
 
         # Copy individual files
         for item in Builder.INDIVIDUAL_FILES_TO_COPY:
-            shutil.copyfile(item, "{0}/{1}".format(Builder.OUTPUT_DIR, item))
+            shutil.copyfile(item, os.path.join(Builder.OUTPUT_DIR, item))
 
-        distutils.dir_util.copy_tree("{0}/{1}".format(Builder.DATA_DIR, Builder.GAMES_DIR), "{0}/{1}".format(Builder.OUTPUT_DIR, Builder.GAMES_DIR))
-        distutils.dir_util.copy_tree("{0}/{1}".format(Builder.DATA_DIR, Builder.GUIDES_DIR), "{0}/{1}".format(Builder.OUTPUT_DIR, Builder.GUIDES_DIR))
+        distutils.dir_util.copy_tree(os.path.join(Builder.DATA_DIR, Builder.GAMES_DIR), os.path.join(Builder.OUTPUT_DIR, Builder.GAMES_DIR))
+        distutils.dir_util.copy_tree(os.path.join(Builder.DATA_DIR, Builder.GUIDES_DIR), os.path.join(Builder.OUTPUT_DIR, Builder.GUIDES_DIR))
 
         self.__generate_master_page()
         self.__generate_static_pages()
@@ -116,7 +116,7 @@ class Builder:
             final_html = self.master_page_html.replace(Builder.CONTENT_PLACEHOLDER, html).replace('@title', g.get('name'))
             filename = g.get_url()
 
-            file_io.write("{0}/{1}".format(Builder.OUTPUT_DIR, filename), final_html)
+            file_io.write(os.path.join(Builder.OUTPUT_DIR, filename), final_html)
  
     def __generate_front_page_game_entries(self):
         featured_html = ''
@@ -130,10 +130,10 @@ class Builder:
             # Regardless of extension, add size
             filename = g.get('screenshot')
 
-            game_image = "{0}/{1}".format(Builder.IMAGES_DIR, filename)
+            game_image = os.path.join(Builder.IMAGES_DIR, filename)
             if not os.path.isfile(game_image):
                 raise(Exception("Can't find image {2}/{0} for game {1}".format(g.get('screenshot'), g.get('name'), Builder.IMAGES_DIR)))
-            html = "<a href='{0}'><img class='img-responsive' src='{1}' /></a>".format(g.get_url(), game_image.replace('{0}/'.format(Builder.DATA_DIR), ''))
+            html = "<a href='{0}'><img class='img-responsive' src='{1}' /></a>".format(g.get_url(), game_image.replace('{0}{1}'.format(Builder.DATA_DIR, os.sep), ''))
 
             platform_html = ""
             for platform_data in g.get('platforms'):
@@ -178,10 +178,10 @@ class Builder:
             regular_html += "</div>"
 
         featured_html = file_io.read(Builder.JUMBOTRON_SNIPPET).replace('@content', featured_html)
-        html = file_io.read("{0}/{1}".format(Builder.OUTPUT_DIR, Builder.INDEX_PAGE))
+        html = file_io.read(os.path.join(Builder.OUTPUT_DIR, Builder.INDEX_PAGE))
         html = self.master_page_html.replace(Builder.CONTENT_PLACEHOLDER, "{0}{1}".format(featured_html, regular_html)).replace('@title', 'Home')
-        file_io.write("{0}/{1}".format(Builder.OUTPUT_DIR, Builder.INDEX_PAGE), html)
-        distutils.dir_util.copy_tree("{0}/.".format(Builder.IMAGES_DIR), "{0}/images".format(Builder.OUTPUT_DIR))
+        file_io.write(os.path.join(Builder.OUTPUT_DIR, Builder.INDEX_PAGE), html)
+        distutils.dir_util.copy_tree(os.path.join(Builder.IMAGES_DIR, "."), os.path.join(Builder.OUTPUT_DIR, "images"))
 
     # Generates static pages from data/static_pages/*.md
     # Converts them into HTML, links them in the header
@@ -195,7 +195,7 @@ class Builder:
             html = self.master_page_html.replace(Builder.CONTENT_PLACEHOLDER, to_html).replace('@title', self.__to_title(self.__get_page_name(p)))
             page_name = self.__get_page_name(p)
 
-            file_io.write("{0}/{1}.html".format(Builder.OUTPUT_DIR, page_name), html)
+            file_io.write(os.path.join(Builder.OUTPUT_DIR, "{0}.html".format(page_name)), html)
 
         print("Generated {0} games and {1} static pages.".format(len(self.games), len(self.pages)))
 
@@ -204,9 +204,9 @@ class Builder:
     # using/abusing this by calling it index.html
     def __generate_master_page(self):
         # Get a list of all static pages. We need links for our header.
-        index_page = file_io.read("{0}/{1}".format(Builder.OUTPUT_DIR, Builder.INDEX_PAGE))
+        index_page = file_io.read(os.path.join(Builder.OUTPUT_DIR, Builder.INDEX_PAGE))
         # Naively, sort alphabetically. That usually makes sense.
-        self.pages = glob.glob("{0}/*.md".format(Builder.STATIC_PAGES_DIR))
+        self.pages = glob.glob(os.path.join(Builder.STATIC_PAGES_DIR, "*.md"))
         self.pages.sort()
 
         navbar_template = file_io.read(Builder.NAVBAR_LINK_SNIPPET)
@@ -226,7 +226,7 @@ class Builder:
      # page file: eg. data/pages/privacy_policy.md
      # returns: 'privacy_policy'
     def __get_page_name(self, markdown_filename):
-        name_start = markdown_filename.rindex('/') + 1
+        name_start = markdown_filename.rindex(os.sep) + 1
         name_stop = markdown_filename.rindex('.md')
         page_name = markdown_filename[name_start:name_stop]
         return page_name
@@ -294,7 +294,7 @@ class Game:
                 raise(Exception("Not sure how to process platform: {0}".format(in_page_data)))
             
             # Common to flash/html5/silverlight
-            template = file_io.read("{0}/snippets/{1}.html".format(template_directory, platform))
+            template = file_io.read(os.path.join(template_directory, "snippets", "{0}.html".format(platform)))
             template = template.replace('@width', str(data['width']))
             template = template.replace('@height', str(data['height']))
 
@@ -315,7 +315,7 @@ class Game:
     # Download links for @downloads, if applicable (desktop only)
     def get_downloadable_platforms_html(self, template_directory, games_directory):
         downloadable_data = self.platform_data(['windows', 'linux', 'mac'])
-        template = file_io.read("{0}/snippets/download_game.html".format(template_directory))
+        template = file_io.read(os.path.join(template_directory, "snippets", "download_game.html"))
         downloads_html = ''
 
         if downloadable_data: # not empty
@@ -326,7 +326,7 @@ class Game:
 
         # If we have something to download, show the download section. (Separate template.)
         if downloads_html: # not empty
-            downloads_section = file_io.read("{0}/snippets/downloads.html".format(template_directory))
+            downloads_section = file_io.read(os.path.join(template_directory, "snippets", "downloads.html"))
             downloads_section = downloads_section.replace('@html', downloads_html)
             return downloads_section
         else:
@@ -353,8 +353,8 @@ class Game:
             name = self.get_url().replace('.html', '')
             ss_html = ''
             for s in self.get('screenshots'):
-                url = "images/{0}/{1}".format(name, s)
-                native_size = Image.open("data/{0}".format(url)).size # (w, h)
+                url = os.path.join("images", name, s)
+                native_size = Image.open(os.path.join("data", url)).size # (w, h)
                 # Scale to 250px. Unless the image is already smaller. Then don't scale.
                 scale = min(1.0 * Builder.MAX_SCREENSHOT_SIZE / native_size[0], 1.0 * Builder.MAX_SCREENSHOT_SIZE / native_size[1], 1)
                 scale_w = int(scale * native_size[0])
