6ec874073d861468f52abc73bfd188a4f1c73282
ashes999 <ashes999@yahoo.com>
2017-08-10 14:41:48 -0400

    Check/throw if PIL isn't available
    Former-commit-id: 6e1a997fd44c7f2c4160e6f3966c28b3bdb561dd

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import src.builder
-src.builder.Builder().build()
+# requires Python 3.4+
+from importlib import util
+spec = util.find_spec('PIL')
+
+if spec is not None:
+    import src.builder
+    src.builder.Builder().build()
+else:
+    raise "Please install the 'pillow' library (pip install pillow)."
